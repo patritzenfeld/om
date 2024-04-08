c023632bc3294689081693694100df006529eef7
patritzenfeld <33265318+patritzenfeld@users.noreply.github.com>
2024-04-08 16:43:04 +0200

    test image constructor

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main (main) where
 
 import OM.Instance
 import Control.Monad.Output
+import Control.Monad.IO.Class(MonadIO(liftIO))
 
 
 
@@ -14,9 +15,11 @@ main = do
 
 
 
-output :: OutputMonad m => Bool -> LangM m
+output :: (MonadIO m, OutputMonad m) => Bool -> LangM m
 output b = do
-  image "a"
+  image $=<< liftIO $ do
+    print "Oops, I did an IO again..."
+    pure "a"
   assertion b $ indent $ do
     paragraph $ translate $ do
       german "Abgebrochen!"
@@ -30,6 +33,9 @@ output b = do
 
 doubleConvert :: LangM (ReportT [Result] IO) -> IO [Result]
 doubleConvert out = do
+  putStrLn "before getting results"
   res <- getResults out
+  putStrLn "after getting results"
   res2 <- getResults $ toOutputMonad $ res
+  putStrLn "after back conversion"
   pure (res2)
